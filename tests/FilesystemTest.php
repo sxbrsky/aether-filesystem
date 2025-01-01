affefb41794b6e8f13f8159c12ea1db011f5c85a
Dominik <dominikszamburski99@gmail.com>
2025-01-01 08:22:23 +0000

    Update copyright date (#11)

--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the aether/aether.
  *
- * Copyright (C) 2024 Dominik Szamburski
+ * Copyright (C) 2024-2025 Dominik Szamburski
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
